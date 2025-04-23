db16b922d4b1860898341105fdb930ee88f23ec7
amr573 <amrooozakaria@gmail.com>
2025-04-23 16:13:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+// let ali ="0"
+// let nuu = 0
+// let mmm ="0"
+// let numn=0
+// let f=0
+// let u=0
+// function sendMessage(){
+//     let name = document.getElementById("na").value;
+//     ali = name 
+//     let num = document.getElementById("num").value;
+//     nuu = num
+//     let message = document.getElementById("fu").value;
+//     mmm=message
+//     let phoneNumber = "97455420652"
+//     numn=phoneNumber
+//     let fullMessage = `الاسم: ${name}%0Aالرقم: ${num}%0Aالرسالة: ${message}`
+//     f=fullMessage
+//     let url =`https://wa.me/${phoneNumber}`
+//     u=url
+// }
+// let send=document.getElementById("send")
+// send.onclick=sendMessage
+
+// window.alert("هل عندك اي استفسار؟ تواصل معنا على واتس اب")
+console.log("can you contact us")
+let c = document.querySelector(".c");
+let b = document.querySelector(".b");
+
+c.addEventListener("click", function() {
+    c.textContent = "عمل جيد";
+});
+
+b.addEventListener("click", function() {
+    b.textContent = "عمل سيئ";
+});
+    
+
+
+let a = document.querySelector(".a");
+let d = document.querySelector(".d");
+
+a.addEventListener("click", function() {
+    a.textContent = "عمل جيد";
+});
+
+d.addEventListener("click", function() {
+    d.textContent = "عمل سيئ";
+});
+    
+let w = document.querySelector(".w");
+let q = document.querySelector(".q");
+
+w.addEventListener("click", function() {
+    w.textContent = "عمل سيئ";
+});
+
+q.addEventListener("click", function() {
+    q.textContent = "عمل سيئ";
+});
+    
+
+
+let l = document.querySelector(".l");
+let u = document.querySelector(".u");
+
+l.addEventListener("click", function() {
+    l.textContent = "عمل سيئ";
+});
+
+u.addEventListener("click", function() {
+    u.textContent = "عمل سيئ";
+});
+    
